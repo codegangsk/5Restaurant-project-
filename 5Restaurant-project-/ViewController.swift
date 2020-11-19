b210aa10f64d0aa3dfb1fd1326fd78ccfedaec65
codegangsk <kjhj0604@gmail.com>
2020-11-19 16:00:14 +0900

    Initial Commit

---
+++ 5Restaurant-project-/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  5Restaurant-project-
+//
+//  Created by Sophie Kim on 2020/11/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
