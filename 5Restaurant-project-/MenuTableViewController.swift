3d490c32efc52809fc094e2abbe94cafcd613282
codegangsk <kjhj0604@gmail.com>
2020-11-24 11:39:01 +0900

    show the selected category as menu table view's title

--- 5Restaurant-project-/MenuTableViewController.swift
+++ 5Restaurant-project-/MenuTableViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class MenuTableViewController: UITableViewController {
+    let menuController = MenuController()
+    var menuItems = [MenuItem]() 
     var category: String!
 }
 
 extension MenuTableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = category.capitalized
+        menuController.fetchMenuItems(forCategory: category) { (menuItems) in
+            if let menuItems = menuItems {
+                self.updateUI(with: menuItems)
+            }
+            
+        }
     }
 }
 
@@ -24,5 +34,10 @@ extension MenuTableViewController {
 }
 
 extension MenuTableViewController {
-   
+    func updateUI(with menuItems: [MenuItem]) {
+        DispatchQueue.main.async {
+            self.menuItems = menuItems
+            self.tableView.reloadData()
+        }
+    }
 }
